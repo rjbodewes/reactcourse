f01fb13185ed560fa372a0a89f1cb80c9eade45f
rjbodewes <rjbodewes@gmail.com>
2022-08-17 23:38:49 +0200

    adding filter 1

--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -1,13 +1,22 @@
 import React from 'react';
-
+import { useState } from 'react';
 import './ExpensesFilter.css';
 
 const ExpensesFilter = () => {
+  
+  const [pickDate, setPickDate] = useState(''); 
+
+  const pickDateHandler = (event) => {
+      //console.log(event.target.value);
+      setPickDate(event.target.value);
+      
+  };
+
   return (
     <div className='expenses-filter'>
       <div className='expenses-filter__control'>
         <label>Filter by year</label>
-        <select>
+        <select onChange={pickDateHandler} value={pickDate}>
           <option value='2022'>2022</option>
           <option value='2021'>2021</option>
           <option value='2020'>2020</option>
