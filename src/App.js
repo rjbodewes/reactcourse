e778c8de95b13fbf56b37afd509c42a7e213fb61
rjbodewes <rjbodewes@gmail.com>
2022-08-21 01:27:48 +0200

    working website now

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useSate } from 'react';
+import React, { useState } from 'react';
 
 import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses/Expenses';
@@ -29,16 +29,14 @@ const DUMMY_EXPENSES = [
 
 
 
-
-
 const App = () => {
-  
   const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
-  
-  
+
 
   const addExpenseHandler = expense => {
-    setExpenses();
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses]; 
+    });
   };
 
   // return React.createElement(
